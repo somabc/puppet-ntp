cbb811bc4c5f6cc3f10456a86141f7a1cdc285a3
root <root@bryanc1.mylabserver.com>
2018-09-22 20:50:28 +0000

    ntp conf

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,18 @@
 #
 # Copyright 2018 Your name here, unless otherwise noted.
 #
-class ntp {
-
-
+class ntp (
+  String $package_name        = $ntp::params::package_name,
+  String $package_ensure      = $ntp::params::package_ensure,
+  String $config_name         = $ntp::params::config_name,
+  String $config_file_mode    = $ntp::params::config_file_mode,
+  Array[String] $servers      = $ntp::params::servers,
+  String $service_ensure      = $ntp::service_ensure,
+  Boolean $service_enable     = $ntp::params::service_enable,
+  Boolean $service_hasrestart = $ntp::params::service_hasrestart,
+  Boolean $service_hasstatus  = $ntp::params::service_hasstatus,
+) inherits ::ntp::params { 
+  class { '::ntp::install': }
+  -> class { '::ntp::config': }
+  ~> class { '::ntp::service': }
 }
